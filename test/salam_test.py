61da18259f362c779446d9c156f550ea5450fa2d
jason.jafari.m@gmail.com <jason.jafari.m@gmail.com>
2020-05-02 16:05:50 -0400

    ssss

---
+++ test/salam_test.py
@@ -0,0 +1,45 @@
+
+
+def fizzBuzz(value):
+    if isMultiplle(value , 3):
+        return "Fizz"
+    if isMultiplle(value,5):
+        return "Buzz"
+
+    return str(value)
+
+def isMultiplle(value , mod):
+    return (value % mod) == 0
+
+
+def checkFizzBuzz(value, expectedRetVal):
+    retVal = fizzBuzz(value)
+    assert retVal == expectedRetVal
+
+
+def test_returns1WithPassedIn():
+    checkFizzBuzz(1, "1")
+
+
+def test_return2With2PassedIn():
+    checkFizzBuzz(2, "2")
+
+
+def test_returnsFizzWith3PassedIn():
+    checkFizzBuzz(3, "Fizz")
+
+
+def test_returnsBuzzWith5pass():
+    checkFizzBuzz(5, "Buzz")
+
+def test_returnsFizzWith6PassedIn():
+    checkFizzBuzz(6,"Fizz")
+    checkFizzBuzz(9,"Fizz")
+
+def test_returnsBuzzWith10PassedIn():
+    checkFizzBuzz(10,"Buzz")
+    checkFizzBuzz(20,"Buzz")
+ 
+def test_returnsFizzBuzzWith15PassedIn():
+    checkFizzBuzz(15,"FizzBuzz")
+    checkFizzBuzz(30,"FizzBuzz")
