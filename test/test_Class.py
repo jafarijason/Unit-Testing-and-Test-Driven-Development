15c7943b0cec1423b7d893fcbe6c9edac856d936
jason.jafari.m@gmail.com <jason.jafari.m@gmail.com>
2020-05-02 23:47:53 -0400

    ffff

---
+++ test/test_Class.py
@@ -0,0 +1,14 @@
+class TestClass:
+    def test_me(self):
+        assert True
+
+    def test_me2(self):
+        assert True
+
+    def not_a_test(self):
+        assert True
+
+
+class MyTestClass():
+    def test_it(self):
+        assert False
---
+++ test/test_file.py
@@ -0,0 +1,9 @@
+def test_me():
+    assert True
+
+def test_me2():
+    assert True
+
+def not_a_test():
+    assert True
+    
